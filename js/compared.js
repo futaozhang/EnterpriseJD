bf4d55f261e385d510a8d8a77e62e4679da0a110
futaozhang <futaozhang@users.noreply.github.com>
2018-05-24 18:39:48 +0800

    采购对比修改

--- js/compared.js
+++ js/compared.js
@@ -1,49 +1,26 @@
 
 
 
-var compandDate=[
-		{
-			"id":"一",
-			"info":[
-				{"id":11},
-				{"id":12}
-			]
-		},
-		{
-			"id":"二",
-			"info":[
-				{"id":21},
-				{"id":23},
-				{"id":23}
-			]
-		},
-		{
-			"id":"三",
-			"info":[
-				{"id":21},
-				{"id":23},
-				{"id":23}
-			]
-		},
-		{
-			"id":3,
-			"info":[
-				{"id":31},
-				{"id":32}
-			]
-		}
-		
-		
-		
-		
-	]
-	
-	
-	var k_tmpl = document.getElementById('k_tempelate').innerHTML;
-	
-	document.getElementById('warp_content').innerHTML=doT.template(k_tmpl)(compandDate);
 
-		$("#Compared").delegate(".ul_num .reduce",'click',function(){
+	
+	//采购方案数据渲染
+	var k_tmpl = document.getElementById('k_tempelate').innerHTML;
+
+	// $.getJSON("./../mockData/compared.json",function(d){
+	// 	console.log(d)
+	
+	// });
+
+	$.getJSON("./../mockData/compared.json",function(item){
+	
+		document.getElementById('warp_content').innerHTML=doT.template(k_tmpl)(item);
+
+	
+	});
+	
+	
+	
+	$("#Compared").delegate(".ul_num .reduce",'click',function(){
 		
 		var nowData=$(this).parent().find("input[type='text']").prop("value");
 		
